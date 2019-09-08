7ff9dffadf257a2236d5796c2f063d6e1e4bd5bd
Rohmanseo <rohmann311@gmail.com>
2019-09-08 18:01:01 +0700

    Membuat routing sederhana dengan Node Js

---
+++ Routing.js
@@ -0,0 +1,26 @@
+var http = require('http');
+
+//Port yang digunakan
+const PORT = 8080;
+
+http.createServer(function(req,res){
+	//REGEX
+	var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+	switch(path){
+		//Arahkan ke homepage
+		case '':
+				res.writeHead(200,{'Content-Type' : 'text/html'});
+				res.end('Homepage');
+				break;
+		//Mengarahkan ke halaman about
+		case '/about' :
+				res.writeHead(200,{'Content-Type' : 'text/html'});
+				res.end('About');
+				break;
+		//Jika halaman tidak ada beri respon 404
+		default :
+				res.writeHead(404,{'Content-Type' : 'text/html'});
+				res.end('Not Found');
+				break;
+	}
+}).listen(PORT);
