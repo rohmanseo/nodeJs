33e68aa05c910bd6f51275949aae36021e664218
Rohmanseo <rohmann311@gmail.com>
2019-09-08 15:55:19 +0700

    Menambahkan respon 200 OK

--- myFirst.js
+++ myFirst.js
@@ -3,6 +3,8 @@ var http = require('http');
 
 //Membuat Server
 http.createServer(function(req,res){
+	//Memberikan respon 200
+	res.writeHead(200,{'Content-Type':'text/html'});
 	//Memberikan respon kepada client yang me request
 	res.write('Halo Dunia.');
 	//Mengakhiri respon
